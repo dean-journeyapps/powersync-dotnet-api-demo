6390e4c8acd7dc72fff593b7c2e3e4e98e05199e
dean-journeyapps <dean@journeyapps.com>
2025-03-27 18:17:06 +0200

    temp test token

--- PowerSync.Api/Controllers/AuthController.cs
+++ PowerSync.Api/Controllers/AuthController.cs
@@ -86,7 +86,8 @@ namespace PowerSync.Api.Controllers
 
             string token = JWT.Encode(payload, rsa, JwsAlgorithm.RS256, headers);
 
-            return Ok(new { token, powersync_url = _config.Url });
+            // return Ok(new { token, powersync_url = _config.Url });
+            return Ok(new { token = "eyJhbGciOiJSUzI1NiIsImtpZCI6InBvd2Vyc3luYy1kZXYtMzIyM2Q0ZTMifQ.eyJzdWIiOiJjYzFiYTRjMy0zYTQyLTQyNTAtOTU5Yi0xNTA4YzQzYWY3MTIiLCJpYXQiOjE3NDMwOTIxMzcsImlzcyI6Imh0dHBzOi8vcG93ZXJzeW5jLWFwaS5qb3VybmV5YXBwcy5jb20iLCJhdWQiOiJodHRwczovLzY3ZTJiZGJlYWIyYzUwOTBjOWM4MjY5Yy5wb3dlcnN5bmMuam91cm5leWFwcHMuY29tIiwiZXhwIjoxNzQzMTM1MzM3fQ.tpsmsBhd-BL59ypUnsL1sh57_qWFw7uGzDe3AJvvCL98bSI4qnFL7ZU4q0yxO2ojHio4PzPWW56NNpN-9yS11XEddT1ldRU6aXjkyzYvzoBinRRIdIo-dQv2d9f_Ae7P_uTvbkCGmAaTmgGAwLDxdx1s_bQXLeQMEyr0I2ESlRuJDngaMOam9lvKAnjvS4m5MdNDZkdm5OLgfzPGpUMlAuxc4YdXH19NBwEYxxejfxt_XEehgPIc4_Jdm52Nn-FJhgN3qxJ9H-Zzn8ij4b1O3B1kQMZ31iY7CUHap48Tibit39iuylt3FhPjLT862YeU5cNqHTjiaGFmn-NPv4ydKA", powersync_url = _config.Url });
         }
 
         [HttpGet("keys")]
