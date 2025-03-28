6adb2e3d4328872e306a7d92014e86cc7c47bd9f
dean-journeyapps <dean@journeyapps.com>
2025-03-28 12:35:46 +0200

    Set default PowerSync URL in Program.cs

--- PowerSync.Api/Program.cs
+++ PowerSync.Api/Program.cs
@@ -66,6 +66,7 @@ if (!string.IsNullOrWhiteSpace(envDatabaseUri))
 }
 
 var envUrl = Environment.GetEnvironmentVariable("POWERSYNC_URL");
+envUrl = "https://67e2bdbeab2c5090c9c8269c.powersync.journeyapps.com";
 if (!string.IsNullOrWhiteSpace(envDatabaseUri))
 {
     builder.Configuration[$"{PowerSyncConfig.SectionName}:Url"] = envUrl;
